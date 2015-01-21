4d6318655ded3f16c954d34aaa40a9183e17157d
Vaclav Pavlin <vpavlin@redhat.com>
2015-01-21 13:23:43 +0100

    Improve build.sh

--- fedora-image-builder/build.sh
+++ fedora-image-builder/build.sh
@@ -5,7 +5,13 @@ set -x
 VM_IMAGE=/tmp/fedora.img
 NAME=fedora-rawhide-base
 KICKSTART=https://git.fedorahosted.org/cgit/spin-kickstarts.git/plain/fedora-docker-base.ks
-REPO=http://kojipkgs.fedoraproject.org/mash/rawhide/x86_64/os/
+INSTALLROOT=http://kojipkgs.fedoraproject.org/mash/rawhide/x86_64/os/
+DOCKER_IMPORT=true
+KEEP_VM=
+REPO=
+KERNEL=
+INITRD=
+BOOT=""
 
 while [ -n "$1" ]; do
     case $1 in
@@ -21,9 +27,32 @@ while [ -n "$1" ]; do
             shift
             KICKSTART=$1
             ;;
+        "--installroot")
+            shift
+            INSTALLROOT=$1
+            ;;
         "--repo")
             shift
             REPO=$1
+            ;;
+        "--kernel")
+            shift
+            KERNEL=$1
+            ;;
+        "--initrd")
+            shift
+            INITRD=$1
+            ;;
+        "--keep-vm")
+            if ! [[ $2 =~ ^--* ]]; then
+                shift
+                KEEP_VM=$1
+            else
+                KEEP_VM="image"
+            fi
+            ;;
+        "--skip-docker")
+            DOCKER_IMPORT=false
             ;;
         *)
             echo "Unknown option $1"
@@ -34,8 +63,9 @@ while [ -n "$1" ]; do
     shift
 done
 
+TMP_DIR=$(mktemp -d /tmp/build-image-XXXXXX)
 if ! [ -e "${KICKSTART}" ]; then
-    TMP_DIR=$(mktemp -d /tmp/build-image-XXXXXX)
+    
     pushd $TMP_DIR &> /dev/null
     curl -O ${KICKSTART} --write-out '%{http_code}\n' | grep -v ^404$ &> /dev/null
     CURL_OUT=$?
@@ -46,10 +76,23 @@ if ! [ -e "${KICKSTART}" ]; then
        echo "Can't find kickstart file: ${KICKSTART}"
        exit 1
     fi
+else
+    cp ${KICKSTART} ${TMP_DIR}
+    KICKSTART=${TMP_DIR}/${KICKSTART##*/}
 fi
 
+[ -z "${REPO}" ] && REPO=${INSTALLROOT}
+
+i=0
+for repo in $(echo ${REPO} | tr "," "\n"); do
+    replace="repo --name=build-$i --baseurl=${repo}"
+    sudo sed -i "s#^%packages.*#${replace}\n&#" ${KICKSTART}
+    i=$(( $i + 1 ))
+done
 
 u=$([ -n "$SUDO_USER" ] && echo $SUDO_USER || echo $USER)
+
+[ -e "${KERNEL}" -a -e "${INITRD}" ] && BOOT="--boot kernel=${KERNEL},initrd=${INITRD}"
 
 virt-install --connect=qemu:///system \
 -n ${NAME} \
@@ -59,22 +102,34 @@ virt-install --connect=qemu:///system \
 --accelerate \
 -v \
 -w bridge:virbr0 \
---disk path=${VM_IMAGE},size=3,format=qcow2 \
--l ${REPO} \
+--disk path=${VM_IMAGE},size=8,format=qcow2 \
+-l ${INSTALLROOT} \
 --nographics \
 --network network:default \
 --console pty \
 --hvm \
 --noreboot \
+$BOOT \
 --initrd-inject=${KICKSTART} \
 -x "ks=file:/${KICKSTART##*/} console=tty0 console=ttyS0,115200"
 #-x "ks=file:/{KS_NAME} console=tty0 console=ttyS0,19200n8"
 
 
-[ $? -eq 0 ] && virt-tar-out -a ${VM_IMAGE} / - | docker import - ${u}/${NAME} 
+[ $? -eq 0 ] && ${DOCKER_IMPORT} && virt-tar-out -a ${VM_IMAGE} / - | docker import - ${u}/${NAME} 
 
 virsh destroy ${NAME}
-virsh undefine ${NAME} --remove-all-storage
+
+if [ -n "${KEEP_VM}" ]; then 
+    if [ ${KEEP_VM} == "image" ]; then
+        virsh undefine ${NAME}
+    elif [ ${KEEP_VM} == "vm" ]; then
+        #TODO: Rewrite this..ugh
+        echo "Keeping VM"
+    fi
+else
+    virsh undefine ${NAME}  --remove-all-storage
+fi
+#virsh undefine ${NAME} # --remove-all-storage
 
 
 
