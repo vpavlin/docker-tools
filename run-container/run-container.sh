591eab42479555495845319404f9a77b21e3446d
Vaclav Pavlin <vpavlin@redhat.com>
2015-02-06 13:38:25 +0100

    Simplify the run script

--- run-container/run-container.sh
+++ run-container/run-container.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 #We need /run and /tmp volumes for fedora, cgroup is needed always
-DEFAULT_VOLUMES="-v /sys/fs/cgroup:/sys/fs/cgroup -v /run -v /tmp"
+DEFAULT_VOLUMES="-v /sys/fs/cgroup:/sys/fs/cgroup -v /var/log/journal:/var/log/journal -v /run -v /tmp"
 #We need to tell systemd that it runs in Docker
 DEFAULT_ENVS="-e container=docker"
 DEFAULT_CMD="/usr/sbin/init"
@@ -10,22 +10,13 @@ DEFAULT_NAME="test"
 
 VOLUMES=${DEFAULT_VOLUMES}
 
-#Create a temp dir to store logs and machine-id
-TMP_DIR=$(mktemp -d /tmp/container-XXXXX)
-echo ${TMP_DIR}
-
-#Prepare journal mount
-LOG_DIR=${TMP_DIR}/logs
-[ -d ${LOG_DIR} ] || mkdir ${LOG_DIR}
-VOLUMES="${VOLUMES} -v ${LOG_DIR}:/var/log/journal"
-
-#Prepare machine-id mount
-MACHINE_FILE=${TMP_DIR}/machine-id
-[ -e ${MACHINE_FILE} ] || touch ${TMP_DIR}/machine-id
-VOLUMES="${VOLUMES} -v ${MACHINE_FILE}:/etc/machine-id"
-
 IMAGE=$1
 [ -z "${IMAGE}" ] && echo "Expecting image name" && exit 1
+
+#Prepare machine-id mount
+MACHINE_FILE=$(mktemp /tmp/container-XXXXX)
+echo ${MACHINE_FILE}
+VOLUMES="${VOLUMES} -v ${MACHINE_FILE}:/etc/machine-id"
 
 #Create a container
 ID=$(docker create --name ${DEFAULT_NAME} ${DEFAULT_OPTS} ${VOLUMES} ${DEFAULT_ENVS} ${IMAGE} ${DEFAULT_CMD})
@@ -35,10 +26,6 @@ shortID=$(echo ${ID} |  cut -c1-32)
 
 #Set machine-id
 echo $shortID > ${MACHINE_FILE}
-
-#Add logs to host's /var/log/journal
-mkdir ${LOG_DIR}/$shortID
-ln -s ${LOG_DIR}/$shortID /var/log/journal/$shortID
 
 
 #Start the container
