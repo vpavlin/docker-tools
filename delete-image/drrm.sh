b2786675ce732af9c588ec29fe85d036982fbaf8
Vaclav Pavlin <vpavlin@redhat.com>
2015-02-25 13:14:26 +0100

    Add Docker Registry ReMove script

---
+++ delete-image/drrm.sh
@@ -0,0 +1,50 @@
+IMAGE=$1
+DEFAULT_ACTION="N"
+
+is_true() {
+    case $1 in 
+        "y"|"Y"|"a"|"A"|"Yes"|"yes")
+            echo 1
+            ;;
+        *)
+            echo 0
+            ;;
+    esac
+}
+
+registry=${IMAGE%%/*}
+IMAGE=${IMAGE#*/}
+
+repo=${IMAGE%%/*}
+image=${IMAGE##*/}
+tag=${IMAGE##*:}
+
+
+if [ "$repo" == "$image" ]; then
+    repo=library
+fi
+
+if [ "$tag" == "$IMAGE" ]; then
+    tag=latest
+fi
+
+uri="$registry/v1/repositories/$repo/${image%%:*}/tags/$tag"
+
+echo $uri
+
+id=$(curl -X GET $uri 2> /dev/null)
+
+echo $id | grep error && exit
+echo -n "Do you really want to untag $id? [y/N]: "
+read action
+
+[ -z "$action" ] && action=$DEFAULT_ACTION
+
+if [ `is_true $action` == "1" ]; then 
+    res=$(curl -X DELETE $uri 2> /dev/null)
+    if [ "$res" == "true" ]; then
+        echo "Image $repo/${image%%:*}:$tag removed from $registry"
+    fi
+
+fi
+
